6ac391beb770b34373a1e37932c520dc8f2959e1
Vlasta Vetra <vlastavetra@gmail.com>
2021-06-04 00:20:59 +0300

    Начинаем программировать

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACES_COUNT: 312,
+  CITIES: ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'],
+  CARDS_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount={Setting.PLACES_COUNT}
+      cities={Setting.CITIES}
+      cardsCount={Setting.CARDS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
